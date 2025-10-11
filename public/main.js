c29461447e4cb09849d8e4b595fb9fd249246e94
bigmat18 <mat.giu2002@gmail.com>
2025-10-11 16:42:43 +0200

    Init Bun env

---
+++ public/main.js
@@ -0,0 +1,6 @@
+// src/main.ts
+var output = document.getElementById("output");
+if (output) {
+  const name = "Matteo";
+  output.textContent = `Ciao ${name}, Bun e TypeScript sono pronti!`;
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const output = document.getElementById("output");
+
+if (output) {
+  const name: string = "Matteo";
+  output.textContent = `Ciao ${name}, Bun e TypeScript sono pronti!`;
+}
