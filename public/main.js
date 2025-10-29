b4113dcfe2cba64d03fba58dcb23b048d13d4369
bigmat18 <mat.giu2002@gmail.com>
2025-10-29 19:00:21 +0100

    Update stack

--- public/main.js
+++
@@ -1,6 +0,0 @@
-// src/main.ts
-var output = document.getElementById("output");
-if (output) {
-  const name = "Matteo";
-  output.textContent = `Ciao ${name}, Bun e TypeScript sono pronti!`;
-}
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-const output = document.getElementById("output");
-
-if (output) {
-  const name: string = "Matteo";
-  output.textContent = `Ciao ${name}, Bun e TypeScript sono pronti!`;
-}
